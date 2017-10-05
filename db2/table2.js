51f65f05c7e189da1201147722fcb6a7f0dc469a
Doris Cheng <dorcheng@umich.edu>
2017-10-05 09:28:27 -0400

    change purple table

--- db2/table2.js
+++ db2/table2.js
@@ -1,6 +1,8 @@
 var Sequelize = require('sequelize')
   var db = require('../index.js')
-  module.exports = db.define('table2',{  
-	col3:{
-		type: Sequelize.col4, }
+  module.exports = db.define('TABLE2',{  
+	COL1:{
+		type: Sequelize.COL2, },
+	COL2:{
+		type: Sequelize.COL3, }
 })
