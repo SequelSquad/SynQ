c731b622c26795159495ffab52cf70095ed4a4a5
Elisabeth Seite <elisabeth.seite@gmail.com>
2017-10-06 10:18:52 -0400

    cleaned up column code - removed all the unnecessary stuff

--- app/components/Column.js
+++ app/components/Column.js
@@ -1,12 +1,7 @@
 import React from "react"
 import {Form, FormGroup, Col, FormControl, Button, Checkbox, ControlLabel, Modal} from "react-bootstrap"
-import {connect} from "react-redux"
-import {removeModal} from "../actions/modalAction"
-import {setModel} from "../actions"
-import update from "react-addons-update"
-import {addColumn} from "../actions"
 
-class Column extends React.Component {
+export default class Column extends React.Component {
 	constructor (props){
 		super(props)
 		this.state = {
@@ -15,7 +10,7 @@ class Column extends React.Component {
 			type: this.props.dataValue ? this.props.dataValue.type : ""
 		}
 		this.onHandleChange = this.onHandleChange.bind(this)
-		this.handleSubmit = this.handleSubmit.bind(this)
+		// this.handleSubmit = this.handleSubmit.bind(this)
 	}
 
 	onHandleChange(evt) {
@@ -23,24 +18,10 @@ class Column extends React.Component {
 		this.setState({[evt.target.name]: evt.target.value})
 	}
 
-	handleSubmit(){
-		//console.log("SAVE STATE", this.state)
-		this.props.onSave(this.state)
-	}
-
 	render() {
 		console.log("COLUMNS PROPS ID I WANT", this.props.id)
 		//console.log("DATAVALUES ON THE COLUMN", this.props.dataValue)
 		return (
-			// <div>
-			// 	<span>
-			// 		<form>
-			// 			<input type="text" name="name" onChange={this.onHandleChange}></input>
-			// 			<input type="text" name="type" onChange={this.onHandleChange}></input>
-			// 			<Button type="button" onClick={this.handleSubmit}>Save</Button>
-			// 		</form>
-			// 	</span>
-			// </div>
 			<div>
 				<span>
 					<FormGroup>
@@ -50,7 +31,6 @@ class Column extends React.Component {
 						<Col sm={10}>
 							<FormControl type="column" placeholder="Enter column name" name="name" defaultValue={this.state.name} onChange = {this.props.onHandleCols(this.state.id)} />
 							<FormControl type="column" placeholder="Enter column type" name="type" defaultValue={this.state.type} onChange = {this.props.onHandleCols(this.state.id)} />
-							<Button bsStyle="info" type="button" onClick={this.handleSubmit}>Save</Button>
 						</Col>
 					</FormGroup>
 				</span>
@@ -59,21 +39,21 @@ class Column extends React.Component {
 	}
 }
 
-const mapStateToProps = (state) => {
-	return {
-		//id: state.currRect,
-		// dataValues: ownProps.dataValues,
-		// onHandleCols: ownProps.onHandleCols
-	}
-}
+// const mapStateToProps = (state) => {
+// 	return {
+// 		//id: state.currRect,
+// 		// dataValues: ownProps.dataValues,
+// 		// onHandleCols: ownProps.onHandleCols
+// 	}
+// }
 
-const mapDispatchToProps = (dispatch) => {
-	return {
-		onSave(state) {
-			console.log("^^^^^^^^^^^^^^",state)
-			dispatch(addColumn(state))
-		}
-	}
-}
+// const mapDispatchToProps = (dispatch) => {
+// 	return {
+// 		onSave(state) {
+// 			console.log("^^^^^^^^^^^^^^",state)
+// 			dispatch(addColumn(state))
+// 		}
+// 	}
+// }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Column)
+// export default connect(mapStateToProps, mapDispatchToProps)(Column)
