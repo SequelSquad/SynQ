1b12277758da89dc3a002199a104064c7db96247
David Ko <dk577@cornell.edu>
2017-10-23 12:05:01 -0400

    removed unnecessary thunk

--- app/actions/modelAction.js
+++ app/actions/modelAction.js
@@ -2,6 +2,7 @@
 export const SET_MODEL = "SET_MODEL"
 export const ADD_TABLE = "ADD_TABLE"
 export const REMOVE_MODEL = "REMOVE_MODEL"
+export const ADD_COLUMN = "ADD_COLUMN"
 // export const REMOVE_MODEL = "REMOVE_MODEL"
 
 //ACTION CREATOR
@@ -10,18 +11,7 @@ export const addTable = table => ({type: ADD_TABLE, table})
 export const removeModel = modelID => ({type: REMOVE_MODEL, modelID})
 
 
-export const addModelThunk = (db) =>
-	dispatch => {
-		loadTables(db)
-			.then((response) => {
-				let rectArr = response.map((table, i) => {
-					return {id: Date.parse(new Date) + i, top: 50*i, left: 50*i }
-				})
-				dispatch(addModel(rectArr))
-			}
-			)
-			.catch(err => console.log(err))
-	}
+
 
 
 
